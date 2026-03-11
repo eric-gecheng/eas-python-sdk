4a34ab341d54c640e55a97bef3eab48d2c698f25
gecheng <eric.gc@alibaba-inc.com>
2026-03-11 16:36:58 +0800

    fix python package name

--- eas_prediction/torchrec_predict_pb2.py
+++ eas_prediction/torchrec_predict_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 _sym_db = _symbol_database.Default()
 
 
-import pytorch_predict_pb2 as pytorch__predict__pb2
+import eas_prediction.pytorch_predict_pb2 as pytorch__predict__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
